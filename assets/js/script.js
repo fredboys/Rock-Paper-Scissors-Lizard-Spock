b29faba9b9e59467e799e6786dda19c011e73a20
fredboys <freddieboys@hotmail.co.uk>
2022-07-20 13:46:48 +0000

    Changed player score and computer score to id instead of class

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const buttons = document.getElementsByClassName("selection");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score")
