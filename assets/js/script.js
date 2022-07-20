9a01904c5d810fe6d97472db0090b6af5a9c5148
fredboys <freddieboys@hotmail.co.uk>
2022-07-20 15:16:54 +0000

    Add event listener to all the buttons and function to play game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,29 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("message");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"]
+
+
+/**
+ * Add event listener to all the buttons
+ */
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+/**
+ * The main game function. Accepts one parameter, which
+ * is the data-choice value of the selected button
+ */
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}-image.jpg`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}-image.jpg`;
+    computerImage.alt = choices[computerChoice];
+}
