2337c25ba2303cfdd3bbcdc9f9c044994e68204e
fredboys <freddieboys@hotmail.co.uk>
2022-07-20 13:50:45 +0000

    Add data choice to weapons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 const buttons = document.getElementsByClassName("selection");
 const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score")
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
