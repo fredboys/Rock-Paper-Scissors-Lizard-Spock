0d9b541229e05e0d154df65b5a85613890ce5ea8
fredboys <freddieboys@hotmail.co.uk>
2022-07-20 13:52:11 +0000

    Add const to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,5 @@ const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("message");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"]
